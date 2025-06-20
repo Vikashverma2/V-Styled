7401d14f9b78ddad17052cd41b7c0ff1ef6feef5
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2025-06-20 23:39:46 +0530

    mens-product-updates

---
+++ src/data/MensProduct.js
@@ -0,0 +1,187 @@
+import men1 from '../assets/men/shirt1.webp'; 
+import men2 from '../assets/men/men-tshirt-1.webp';  
+import men3 from '../assets/men/shoes1.webp';  
+import men4 from '../assets/men/jeans1.webp';  
+import men5 from '../assets/men/men-jacket-1.webp';  
+import men6 from '../assets/men/shirt2.webp'; 
+import men7 from '../assets/men/men-tshirt-2.webp';  
+import men8 from '../assets/men/shoes2.webp';  
+import men9 from '../assets/men/jeans2.webp';  
+import men10 from '../assets/men/men-jacket-2.webp'; 
+import men11 from '../assets/men/shirt3.webp'; 
+import men12 from '../assets/men/men-tshirt-3.webp';  
+import men13 from '../assets/men/shoes3.webp';  
+import men14 from '../assets/men/jeans3.webp';  
+import men15 from '../assets/men/men-jacket-3.webp';  
+import men16 from '../assets/men/shirt5.webp'; 
+import men17 from '../assets/men/men-tshirt-4.webp';  
+import men18 from '../assets/men/shoes4.webp';  
+import men19 from '../assets/men/jeans4.webp';  
+import men20 from '../assets/men/shirt6.webp'; 
+import men21 from '../assets/men/men-tshirt-5.webp';  
+import men22 from '../assets/men/shoes4.webp';  
+import men23 from '../assets/men/jeans5.webp';  
+  
+
+
+
+
+
+
+
+
+
+
+let mens = [
+  { 
+    id: 1,
+    name:  "Men's Casual Shirt",
+    category: "mens",   
+    image: men1,
+    new_price: 449.99,
+    old_price: 699.99,
+ },
+ {                           
+
+    id: 2,
+    name:  "Men's Casual T-Shirt",
+    category: "mens",   
+    image: men2,
+    new_price: 300.99,
+    old_price: 399.99,
+ },
+ { 
+    id: 3,
+    name:  "Men's Shoes",
+    category: "mens",   
+    image: men3,
+    new_price: 799.99,
+    old_price: 999.99,
+ },
+ { 
+    id: 4,
+    name:  "baggy Jeans",
+    category: "mens",   
+    image: men4,
+    new_price: 1299.99,
+    old_price: 1699.99,
+ },
+ { 
+    id: 5,
+    name:  "Men's Jacket",
+    category: "mens",   
+    image: men5,
+    new_price: 2199.99,
+    old_price: 2999.99,
+ },
+ { 
+    id: 6,
+    name:  "Men's Casual Shirt",
+    category: "mens",   
+    image: men6,
+    new_price: 449.99,
+    old_price: 699.99,
+ },                            
+ { 
+    id: 7,
+    name:  "Men's Casual T-Shirt",
+    category: "mens",   
+    image: men7,
+    new_price: 300.99,
+    old_price: 399.99,
+ },
+ { 
+    id: 8,
+    name:  "Men's Shoes",
+    category: "mens",   
+    image: men8,
+    new_price: 799.99,
+    old_price: 999.99,
+ },
+ { 
+    id: 9,
+    name:  "baggy Jeans",
+    category: "mens",   
+    image: men9,
+    new_price: 1299.99,
+    old_price: 1699.99,
+ },
+ { 
+    id: 10,
+    name:  "Men's Jacket",
+    category: "mens",   
+    image: men10,
+    new_price: 2199.99,
+    old_price: 2999.99,
+ },
+ { 
+    id: 11,
+    name:  "Men's Casual Shirt",
+    category: "mens",   
+    image: men11,
+    new_price: 449.99,
+    old_price: 699.99,
+ },                            
+ { 
+    id: 12,
+    name:  "Men's Casual T-Shirt",
+    category: "mens",   
+    image: men12,
+    new_price: 300.99,
+    old_price: 399.99,
+ },
+ { 
+    id: 13,
+    name:  "Men's Shoes",
+    category: "mens",   
+    image: men13,
+    new_price: 799.99,
+    old_price: 999.99,
+ },
+ { 
+    id: 14,
+    name:  "baggy Jeans",
+    category: "mens",   
+    image: men14,
+    new_price: 1299.99,
+    old_price: 1699.99,
+ },
+ { 
+    id: 15,
+    name:  "Men's Casual Shirt",
+    category: "mens",   
+    image: men15,
+    new_price: 449.99,
+    old_price: 699.99,
+ },                            
+ { 
+    id: 16,
+    name:  "Men's Casual T-Shirt",
+    category: "mens",   
+    image: men16,
+    new_price: 300.99,
+    old_price: 399.99,
+ },
+ { 
+    id: 17,
+    name:  "Men's Shoes",
+    category: "mens",   
+    image: men17,
+    new_price: 799.99,
+    old_price: 999.99,
+ },
+ { 
+    id: 18,
+    name:  "baggy Jeans",
+    category: "mens",   
+    image: men18,
+    new_price: 1299.99,
+    old_price: 1699.99,
+ },
+
+ 
+    
+          
+];
+
+export default mens;
--- src/pages/men/Men.jsx
+++ src/pages/men/Men.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import banner from '../../assets/mens-banner.png'
+import './Men.css'
+
+
 
 export const Men = () => {
   return (
-    <div>
-      
-      
+    <div className='men-container'>
+      <img src={banner} alt="Mens-Banner" />
+        
     </div>
   )
 }
